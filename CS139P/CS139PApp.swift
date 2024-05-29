fa6e87283a3848d3cb048502342a0b9cee3b7a8b
david.chukwuemeka <davechuks63@gmail.com>
2024-05-29 14:54:20 -0230

    Initial Commit

---
+++ CS139P/CS139PApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS139PApp.swift
+//  CS139P
+//
+//  Created by Student on 2024-05-29.
+//
+
+import SwiftUI
+
+@main
+struct CS139PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS139PUITests/CS139PUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CS139PUITestsLaunchTests.swift
+//  CS139PUITests
+//
+//  Created by Student on 2024-05-29.
+//
+
+import XCTest
+
+final class CS139PUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
